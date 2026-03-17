5a011f17ddb009d7c2c2b5831f73d80aaca1741e
Pokol <petrenyio@gmail.com>
2026-03-17 21:17:46 +0100

    függvényesítés

--- youtube_api.py
+++ youtube_api.py
@@ -1,22 +1,28 @@
 import googleapiclient.discovery
+from googleapiclient.errors import HttpError
 
 api_service_name = "youtube"
 api_version = "v3"
-
 DEVELOPER_KEY = "AIzaSyBgcbvO3WjGStGAofh-rAXlTVOKKQcFs9k"
 
 youtube = googleapiclient.discovery.build(
     api_service_name, api_version, developerKey = DEVELOPER_KEY)
 
-request = youtube.search().list(
-    part="id, snippet",
-    type="video",
-    order="date",
-    channelId="UCdPhEv5wiw2uK6J0_wkc-RA",
-    maxResults=1,
-    fields="items(id(videoId),snippet(publishedAt,channelId,channelTitle,title,description))"
-)
+def request1():
+    try:
+        print("Hívás indítása...")
+        request = youtube.search().list(
+            part="id, snippet",
+            type="video",
+            order="date",
+            channelId="UCdPhEv5wiw2uK6J0_wkc-RA",
+            maxResults=1,
+            fields="items(id(videoId),snippet(publishedAt,channelId,channelTitle,title,description))"
+        )
+        print(f"A hívás sikeres volt!")
+        response = request.execute()
+        return response
+    except HttpError as e:
+        print(f"Nem sikerült a hívás: {e}")
 
-response = request.execute()
 
-print(response)
