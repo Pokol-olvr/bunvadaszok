59c91d80b4387c9a325fc5b8217e61dea51563d1
Pokol <petrenyio@gmail.com>
2026-03-18 07:13:32 +0100

    titkosítás köszi github

--- src/youtube_api.py
+++ src/youtube_api.py
@@ -1,9 +1,15 @@
 import googleapiclient.discovery
 from googleapiclient.errors import HttpError
 
+
+import_file = r"C:\pityon100\valami\src\key.txt"
+with open(import_file,"r") as api_key:
+    secret = api_key.read()
+    print(secret)
+
 api_service_name = "youtube"
 api_version = "v3"
-DEVELOPER_KEY = "AIzaSyBgcbvO3WjGStGAofh-rAXlTVOKKQcFs9k"
+DEVELOPER_KEY = secret
 
 youtube = googleapiclient.discovery.build(
     api_service_name, api_version, developerKey = DEVELOPER_KEY)
@@ -25,4 +31,4 @@ def request1():
     except HttpError as e:
         print(f"Nem sikerült a hívás: {e}")
 
-
+request1()
