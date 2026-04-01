ff58450800b1016d1f5756d7c8add30b506acdd0
Pokol <petrenyio@gmail.com>
2026-04-01 20:36:18 +0200

    api javítás, json file író

--- src/comparator.py
+++ src/comparator.py
@@ -1,10 +1,15 @@
 from dateutil import parser
+from pathlib import Path
 import json
+from youtube_api import request1
+
+json_utvonal = Path("src") / ("video.json")
+
 
 def json_olvaso():
     try:
         print("File megnyitása...")
-        with open(r"/home/runner/work/bunvadaszok/bunvadaszok/src/video.json", "r", encoding="utf-8") as file: 
+        with open(json_utvonal, "r", encoding="utf-8") as file: 
             data = json.load(file)
         print("Sikeres megnyitás!")
         return data
@@ -16,23 +21,21 @@ def json_olvaso():
 def json_atiro(aktualis_json):
     try:
         print("File átírása...")
-        with open(r"/home/runner/work/bunvadaszok/bunvadaszok/src/video.json", "w", encoding="utf-8") as file: 
-            file.write(aktualis_json)
+        with open(json_utvonal, "w", encoding="utf-8") as file: 
+            file.write(json.dumps(aktualis_json, indent=4,separators=(", ", ": "),ensure_ascii=False))
         print("Sikeres átírás!")
     except FileNotFoundError as e:
         print(f"Nem találom a filet te bolond: {e}")
     except TypeError as e:
         print(f"Nem megfelelő file-t akarsz megynitni öcskös: {e}")
 
-def date_parse(json):
-    json = json_olvaso()
-    ido_z = json["items"][0]["snippet"]["publishedAt"]
+def date_parse(nyers_json):
+    ido_z = nyers_json["items"][0]["snippet"]["publishedAt"]
     ido = parser.parse(ido_z)
     return ido
 
 def osszehasonlito(tarolt_ido, aktualis_ido):
     if tarolt_ido < aktualis_ido:
-        True
+        return True
     else:
-        False
-
+        return False
